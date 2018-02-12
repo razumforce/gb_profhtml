12a9927d9d8a0f61d1edc6b12719f871522bb86d
razumforce <razum.force@gmail.com>
2018-02-12 05:52:27 +0300

    productbypage feature - pagination on product.html working

--- kovorotny-g-work/js/brand_product.js
+++ kovorotny-g-work/js/brand_product.js
@@ -15,50 +15,6 @@ function productPriceSliderInit() {
   $('#product-price2').val('$' + $('#product-price-slider').slider('values', 1));
 }
 
-function productItemsLoad(page) {
-  $.get({
-      url: './serverdata/product' + page + '.json',
-      dataType: 'json',
-      success: function (data) {
-          if (data.result) {
-            productPaginationUpdate(page, data.total);
-            $('.product-items__item').each(function(index) {
-              $(this).attr('data-id', data.items[index].id);
-              $(this).children('img').attr('src', data.items[index].pic);
-              $(this).children('img').attr('alt', data.items[index].pic.split('/').pop());
-              $(this).children('p.product-items__item_desc').text(data.items[index].name);
-              $(this).children('p.product-items__item_price').text(data.items[index].price);
-            });
-          } else {
-            console.log('SOMETHING WENT WRONG, ERROR MESSAGE: ' + data.message);
-          }
-          
-      }
-  });
-}
-
-function productPaginationUpdate(activePage, qty) {
-  var totalPages = parseInt(qty / 9) + 1;
-  $('#product-pagination').html('');
-  if (activePage == 1) {
-    $('#product-pagination').append('<span>&lt;</span>');
-  } else {
-    $('#product-pagination').append('<span class="product-choice__control-active">&lt;</span>');
-  }
-  for (var i = 1; i <= totalPages; i++) {
-    if (i == activePage) {
-      $('#product-pagination').append('<span class="product-choice__pag-active">' + i + '</span>');
-    } else {
-      $('#product-pagination').append('<span>' + i + '</span>');
-    }
-  }
-  if (activePage == totalPages) {
-    $('#product-pagination').append('<span>&gt;</span>');
-  } else {
-    $('#product-pagination').append('<span class="product-choice__control-active">&gt;</span>');
-  }
-}
-
 function productLeftNavInit() {
   $('.product__left-nav').first().children('.product__left-nav__head').each(function(index) {
     if ($(this).hasClass('product__left-nav__active')) {
